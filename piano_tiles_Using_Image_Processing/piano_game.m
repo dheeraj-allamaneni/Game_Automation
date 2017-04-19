1d48f253b6548423632670a5b158a88ed8085ef6
Dheeraj allamaneni <a.dheeraj.95@gmail.com>
2017-04-19 11:48:51 +0530

    Create piano_game.m
    Run this code in Matlab after installation of ADB tool,this code process the image and simulates the touch through ADB tool.

---
+++ piano_tiles_Using_Image_Processing/piano_game.m
@@ -0,0 +1,74 @@
+
+%%Taking the screenshot in the Android phone & saving that image(png format)
+    system('adb shell screencap -p /sdcard/screen2.png');
+
+%%	Copying image to our working directory
+    system('adb pull /sdcard/screen2.png');
+    
+% % reading the image
+%% This command reads the image from a graphics file and stores it in a variable in the matrix form. The syntax for the imread operation is shown below
+
+im=imread('screen2.png');
+imshow(im);
+% % storing the notes of a song in a matrix
+mat1=['C','C','D','C','F', 'E','C','C','D','C','G','F', 'C', 'C', 'K', 'A', 'F', 'E','D', '5', '5','A', 'F', 'G', 'E' ];
+for a = 1:25
+    switch mat1(a)
+ case 'C'
+     % % Simulating the touch with the coordinates
+       cmd=['adb shell input swipe 64 621 65 622 '];
+system(cmd);  
+      
+   case '1'
+       cmd=['adb shell input swipe 128 371 129 372 '];
+system(cmd);
+      
+   case 'D'
+       cmd=['adb shell input swipe 181 605 182 606 '];
+system(cmd);  
+      
+   
+   case 'E'
+       cmd=['adb shell input swipe 328 596 329 597 '];
+system(cmd);
+      
+   case 'F'
+       cmd=['adb shell input swipe 435 588 436 589 '];
+system(cmd);
+      
+   case 'G'
+       cmd=['adb shell input swipe 563 609 564 610 '];
+system(cmd);
+      
+   case 'A'
+       cmd=['adb shell input swipe 688 604 689 605 '];
+system(cmd);
+     
+   case 'B'
+       cmd=['adb shell input swipe 837 585 838 586 '];
+system(cmd);
+      
+     case '2'
+         cmd=['adb shell input swipe 428 263 429 264 '];
+system(cmd);
+      
+       case '3'
+           cmd=['adb shell input swipe 362 497 363 498 '];
+system(cmd);
+     
+      case '4'
+          cmd=['adb shell input swipe 377 629 378 630 '];
+system(cmd);
+      
+       case '5'
+           cmd=['adb shell input swipe 365 791 366 792 '];
+system(cmd);
+      case 'K'
+           cmd=['adb shell input swipe 965 574 966 575 '];
+system(cmd);
+        
+   
+   otherwise
+      x=0; y=0;
+    end
+end
